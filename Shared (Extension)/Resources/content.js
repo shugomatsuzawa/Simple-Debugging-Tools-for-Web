e36c6e9a3ce8f99c079dc77a979e9d70085b5cf9
shugomatsuzawa <50859459+shugomatsuzawa@users.noreply.github.com>
2024-02-25 01:50:31 +0900

    未使用ファイルの削除

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,11 +1,3 @@
-//browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-//    console.log("Received response: ", response);
-//});
-//
-//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//    console.log("Received request: ", request);
-//});
-
 function exitAlert() {
     browser.storage.local.get(['alertEnable', 'alertHostname'], function (result) {
         const savedAlertEnable      = result.alertEnable;
