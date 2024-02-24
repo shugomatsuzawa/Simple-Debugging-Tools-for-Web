2fcc34c530cd62a305de62ab8960d3b81504c78a
shugomatsuzawa <50859459+shugomatsuzawa@users.noreply.github.com>
2024-02-25 01:11:12 +0900

    離脱アラート実装

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,44 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+//browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//    console.log("Received response: ", response);
+//});
+//
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    console.log("Received request: ", request);
+//});
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+function exitAlert() {
+    browser.storage.local.get(['alertEnable', 'alertHostname'], function (result) {
+        const savedAlertEnable      = result.alertEnable;
+        const savedAlertHostname    = result.alertHostname;
+        if(savedAlertEnable && savedAlertHostname){
+            console.log('Saved URL retrieved:', savedAlertHostname);
+            
+            if( location.hostname != savedAlertHostname){
+                console.log('⚠︎ ' + savedAlertHostname + 'ではありません');
+                // 要素を作成
+                const alert_elm = document.createElement('div');
+                alert_elm.className = 'sm-sdtfw-alert-elm';
+                
+                // Textを追加
+                alert_elm.textContent = '⚠︎ ' + savedAlertHostname + 'ではありません';
+                
+                // 親要素の最後の子要素として追加する
+                document.body.appendChild(alert_elm);
+                
+                // 既存のmeta要素を変更する
+                const existingMeta = document.querySelector('meta[name="theme-color"]');
+                if (existingMeta) {
+                    existingMeta.remove();
+                }
+                // meta要素を新しく追加する
+                const themeColor = document.createElement('meta');
+                themeColor.name = 'theme-color';
+                themeColor.content = '#FF3B30';
+                document.head.appendChild(themeColor);
+            }
+        }
+    });
+}
+
+// 既存のページ読み込み時に実行
+exitAlert();
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,35 @@
-console.log("Hello World!", browser);
+const alertEnable   = document.getElementById('alert_enable');
+const alertHostname = document.getElementById('alert_hostname');
+const alertSave     = document.getElementById('alert_save');
+
+// ポップアップ開いた時に設定内容表示
+browser.storage.local.get(['alertEnable', 'alertHostname'], function (result) {
+    const savedAlertEnable      = result.alertEnable;
+    const savedAlertHostname    = result.alertHostname;
+    alertEnable.checked = savedAlertEnable;
+    alertHostname.value = savedAlertHostname;
+});
+
+// 保存ボタンクリック
+document.addEventListener('DOMContentLoaded', function () {
+    alertSave.addEventListener('click', function () {
+        // 入力されたURLをブラウザストレージに保存する
+        const alertEnableValue      = alertEnable.checked;
+        const alertHostnameValue    = alertHostname.value;
+        browser.storage.local.set({
+            'alertEnable': alertEnableValue,
+            'alertHostname': alertHostnameValue
+        }, function () {
+            console.log('URL and Toggle State saved:', alertHostnameValue, alertEnableValue);
+            
+            // 現在のアクティブなタブを取得する
+            browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                const activeTab = tabs[0];
+                // アクティブなタブをリロードする
+                browser.tabs.reload(activeTab.id);
+                // popupを閉じる
+                window.close();
+            });
+        });
+    });
+});
